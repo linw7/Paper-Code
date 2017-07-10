7639b0af6ad65b3bf4df0728b045ea7b81ded30b
INX <linw7@mail2.sysu.edu.cn>
2017-07-10 13:27:18 +0800

    modify

--- code/29.栈的压入弹出序列.cpp
+++ code/29.栈的压入弹出序列.cpp
@@ -9,15 +9,20 @@ bool is_pop_order(const int *push_order, const int *pop_order, int length) {
         int i_pop = 0;
         int i_push = 0;
         stack<int> stack;
-        for (int i = 0; i < length; ++i) {
+        // 未pop完数据
+        while(i_pop != length) {
+            // 栈空则按入栈顺序一直压栈直到栈顶压入第一个出栈序首元素
             while (((stack.empty()) || stack.top() != pop_order[i_pop]))
                 stack.push(push_order[i_push++]);
+            // 不存在出栈序，返回false，否则栈顶元素出栈
             if (stack.top() != pop_order[i_pop])
-                break;
-            stack.pop();
-            ++i_pop;
+                return false;
+            else {
+                stack.pop();
+                ++i_pop;
+            }
         }
-        if (stack.empty() && i_pop == length)
+        if (stack.empty())
             is_order = true;
     }
     return is_order;
@@ -27,7 +32,7 @@ int main() {
     int pop_order_1[5] = {4, 5, 3, 2, 1};
     int pop_order_2[5] = {4, 3, 5, 1, 2};
     int pop_order_3[5] = {5, 4, 3, 2, 1};
-    cout << is_pop_order(push_order, pop_order_1, (sizeof(push_order) / sizeof(int))) << endl;
-    cout << is_pop_order(push_order, pop_order_2, (sizeof(push_order)) / sizeof(int))<< endl;
-    cout << is_pop_order(push_order, pop_order_3, (sizeof(push_order)) / sizeof(int))<< endl;
+    cout << is_pop_order(push_order, pop_order_1, (sizeof(pop_order_1) / sizeof(int))) << endl;
+    cout << is_pop_order(push_order, pop_order_2, (sizeof(pop_order_2)) / sizeof(int))<< endl;
+    cout << is_pop_order(push_order, pop_order_3, (sizeof(pop_order_3)) / sizeof(int))<< endl;
 }
